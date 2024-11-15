8f5ce11ad4130b9e1b84b4c582db80870327ec9f
Patrik Mitterpach <patrik@mitterpach.com>
2024-11-15 19:16:39 +0100

    Daily News Commit

--- orbit/mapping.js
+++ orbit/mapping.js
@@ -2,27 +2,36 @@ const image = document.getElementById('image');
 const canvas = document.getElementById('overlay');
 const ctx = canvas.getContext('2d');
 
-GLOBAL_Y_OFFSET = 0
-GLOBAL_X_OFFSET = 0
+LATITUDE_OFFSET = 2.2
+LONGITUDE_OFFSET = -8.5
 
 image.onload = () => {
     canvas.width = image.width;
     canvas.height = image.height;
 
     // Bratislava
-    drawPointByCoordinates(48.0991776, 16.9519043)
+    drawPointByCoordinates(48.0991776, 16.9519043, 4)
 
     // Buenos Aires
-    drawPointByCoordinates( -34.6483135,-58.579101)
+    drawPointByCoordinates( -34.6483135,-58.579101, 4)
 
     // Guinean Gulf
-    drawPointByCoordinates(0, 0)
+    drawPointByCoordinates(0, 0, 4)
+
+    // for (i = -180; i < 190; i+=2) {
+    //     for (j = -90; j < 90; j+=5) {
+    //         drawPointByCoordinates(j, i, 2 )
+    //         if (j == 0) {
+    //             drawPointByCoordinates(j, i, 4 )
+    //         }
+    //     }
+    // }
 
 
 
 };
 
-function drawPointByCoordinates(latitude, longitude) {
+function drawPointByCoordinates(latitude, longitude, size=6) {
     // latitude    = 41.145556; // (φ)
     // longitude   = -73.995;   // (λ)
 
@@ -30,10 +39,10 @@ function drawPointByCoordinates(latitude, longitude) {
     mapHeight   = canvas.height;
 
     // get x value
-    x = (longitude+180)*(mapWidth/360)
+    x = (longitude+180+LONGITUDE_OFFSET)*(mapWidth/360)
 
     // convert from degrees to radians
-    latRad = latitude*Math.PI/180;
+    latRad = (latitude+LATITUDE_OFFSET)*Math.PI/180;
 
     // get y value
     mercN = Math.log(Math.tan((Math.PI/4)+(latRad/2)));
@@ -41,6 +50,6 @@ function drawPointByCoordinates(latitude, longitude) {
         
     ctx.fillStyle = '#ae5d40';
     ctx.beginPath();
-    ctx.arc(x, y, 6, 0, Math.PI * 2); // A small circle as a point
+    ctx.arc(x, y, size, 0, Math.PI * 2); // A small circle as a point
     ctx.fill();
 }
